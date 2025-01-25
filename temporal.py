8a93774618829c20a16136fdfc8f168b23a1d3ad
d3da <daandieperink@gmail.com>
2025-01-25 16:10:33 +0100

    Enable profiler and log config to wandb

--- temporal.py
+++ temporal.py
@@ -24,17 +24,21 @@ def main():
                                                        monitor='hp_metric',
                                                        mode='min',
                                                        filename='{epoch}-{step}-{hp_metric}')
-    # profiler = pl.profilers.AdvancedProfiler(dirpath='.', filename='profiler_repor')
+    profiler = pl.profilers.AdvancedProfiler(dirpath='.', filename='temporal_profiler_report')
     logger = pl.loggers.WandbLogger(name='CondFoleyGen',
                                     offline=True,
                                     save_dir='wandb_logs',
                                     project='CondFoleyGen',
-                                    log_model=False)
+                                    log_model=False,
+                                    config=OmegaConf.to_object(conf))
+    logger.watch(model)
+
     trainer = pl.Trainer(accelerator='gpu',
                          devices=1,
                          # fast_dev_run=10,
-                         profiler='simple',
+                         profiler=profiler,
                          logger=logger,
+                         log_every_n_steps=1,
                          callbacks=[checkpoint_callback])
 
     trainer.fit(model, data)
