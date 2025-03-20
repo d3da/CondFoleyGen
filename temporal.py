4f003a301b0a2f69797a59afd3aca72f141406f4
d3da <daandieperink@gmail.com>
2025-03-20 16:41:26 +0100

    Set wandb run name and desc. in config file

--- temporal.py
+++ temporal.py
@@ -19,18 +19,20 @@ def main():
     conf = OmegaConf.load('configs/temporal/temporal.yaml')
     model = instantiate_from_config(conf.model)
     data = instantiate_from_config(conf.data)
+    wandb_conf = conf.wandb
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(save_top_k=5,
                                                        monitor='hp_metric',
                                                        mode='min',
                                                        filename='{epoch}-{step}-{hp_metric}')
     profiler = pl.profilers.AdvancedProfiler(dirpath='.', filename='temporal_profiler_report')
-    logger = pl.loggers.WandbLogger(name='CondFoleyGen',
+    logger = pl.loggers.WandbLogger(#name='CondFoleyGen',
                                     offline=True,
                                     save_dir='wandb_logs',
                                     project='CondFoleyGen',
                                     log_model=False,
-                                    config=OmegaConf.to_object(conf))
+                                    config=OmegaConf.to_object(conf),
+                                    **wandb_conf)
     logger.watch(model)
 
     trainer = pl.Trainer(accelerator='gpu',
