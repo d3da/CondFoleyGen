42edb0e47fea1f0c93db394b76156d349e2a7d29
d3da <daandieperink@gmail.com>
2025-04-02 19:03:51 +0200

    Fix video/audio always starting at t=0 instead of random location

--- foley2025/data/greatest_hits.py
+++ foley2025/data/greatest_hits.py
@@ -577,6 +577,11 @@ class GreatestHitEmbeddingSequence(pl.LightningModule):
         start_a, end_a = 0, new_sequence_length
         start_b, end_b = shift, new_sequence_length + shift
 
+        absolute_shift = random.randint(0, original_sequence_length - new_sequence_length - shift + 1)
+
+        start_a, end_a = start_a + absolute_shift, end_a + absolute_shift
+        start_b, end_b = start_b + absolute_shift, end_b + absolute_shift
+
         if bool(random.getrandbits(1)):
             return (start_a, end_a), (start_b, end_b)
         else:
