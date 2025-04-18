058bd3f6212306922690fb1f6a0366b1e3902577
d3da <daandieperink@gmail.com>
2025-04-18 17:04:53 +0200

    Decoder v0.2, add a lot more k=3,s=1 convolutional layers

--- foley2025/decoder/model.py
+++ foley2025/decoder/model.py
@@ -7,11 +7,14 @@ class ResidualUpsampling1d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=5, stride=2):
         super().__init__()
         self.conv = nn.Sequential(
+            nn.Conv1d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(in_channels),
+            nn.SiLU(),
             nn.ConvTranspose1d(in_channels, out_channels,
                                kernel_size=kernel_size, stride=stride,
                                padding=2, output_padding=1),
             nn.BatchNorm1d(out_channels),
-            nn.SiLU()
+            nn.SiLU(),
         )
         self.residual = nn.Sequential(
             nn.ConvTranspose1d(in_channels, out_channels,
@@ -28,6 +31,10 @@ class ResidualHeightUpsampling2d(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels,
+                      kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(in_channels),
+            nn.SiLU(),
             nn.ConvTranspose2d(in_channels, out_channels,
                                kernel_size=5, stride=(2, 1),
                                padding=2, output_padding=(1, 0)),
@@ -49,9 +56,12 @@ class ResidualConv2d(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=(5, 7), padding='same'),
+            nn.Conv2d(in_channels, in_channels, kernel_size=(5, 7), padding='same'),
+            nn.BatchNorm2d(in_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding='same'),
             nn.BatchNorm2d(out_channels),
-            nn.SiLU()
+            nn.SiLU(),
         )
         if in_channels != out_channels:
             self.residual = nn.Sequential(
@@ -65,28 +75,66 @@ class ResidualConv2d(nn.Module):
         # x: (B, C_in, H, W)
         return self.conv(x) + self.residual(x)
 
+class ResidualConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(in_channels, in_channels, kernel_size=5, padding='same'),
+            nn.BatchNorm1d(in_channels),
+            nn.SiLU(),
+            nn.Conv1d(in_channels, out_channels, kernel_size=3, padding='same'),
+            nn.BatchNorm1d(out_channels),
+            nn.SiLU(),
+        )
+        if in_channels != out_channels:
+            self.residual = nn.Sequential(
+                nn.Conv1d(in_channels, out_channels, kernel_size=1),
+                nn.BatchNorm1d(out_channels)
+            )
+        else:
+            self.residual = nn.BatchNorm1d(out_channels)
+
+    def forward(self, x):
+        # x: (B, C_in, H, W)
+        return self.conv(x) + self.residual(x)
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.input_padding = nn.ConstantPad1d(1, 0)  # L: 30 -> 32
+        self.input_padding = nn.ConstantPad1d((1, 1), 0)  # L: 30 -> 32
 
         self.temporal_upsampling = nn.Sequential(
-            ResidualUpsampling1d(in_channels=768, out_channels=1024),  # L: 32 -> 64
+            ResidualConv1d(in_channels=768, out_channels=768),
+            ResidualConv1d(768, 1024),
+            ResidualUpsampling1d(in_channels=1024, out_channels=1024),  # L: 32 -> 64
+            ResidualConv1d(1024, 1024),
             ResidualUpsampling1d(in_channels=1024, out_channels=1024),  # L: 64 -> 128
+            ResidualConv1d(1024, 1024),
             ResidualUpsampling1d(in_channels=1024, out_channels=1024),  # L: 128 -> 256
+            ResidualConv1d(1024, 1024),
             ResidualUpsampling1d(in_channels=1024, out_channels=1024),  # L: 256 -> 512
+            ResidualConv1d(1024, 1024),
             ResidualUpsampling1d(in_channels=1024, out_channels=1024),  # L: 512 -> 1024
+            ResidualConv1d(1024, 1024),
+            ResidualConv1d(1024, 1024),
         )
 
         self.spatial_upsampling = nn.Sequential(
+            ResidualConv2d(1024, 1024),
             ResidualHeightUpsampling2d(in_channels=1024, out_channels=512), # H: 1 -> 2
+            ResidualConv2d(512, 512),
             ResidualHeightUpsampling2d(in_channels=512, out_channels=256), # H: 2 -> 4
+            ResidualConv2d(256, 256),
             ResidualHeightUpsampling2d(in_channels=256, out_channels=128), # H: 4 -> 8
+            ResidualConv2d(128, 128),
             ResidualHeightUpsampling2d(in_channels=128, out_channels=64),  # H: 8 -> 16
+            ResidualConv2d(64, 64),
             ResidualHeightUpsampling2d(in_channels=64, out_channels=32),  # H: 16 -> 32
+            ResidualConv2d(32, 32),
             ResidualHeightUpsampling2d(in_channels=32, out_channels=16),  # H: 32 -> 64
+            ResidualConv2d(16, 16),
             ResidualHeightUpsampling2d(in_channels=16, out_channels=8),   # H: 64 -> 128
+            ResidualConv2d(8, 8),
         )
 
         self.conv2d = nn.Sequential(
