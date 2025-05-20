8f60eb0297cddc259d300cb7bf6f7f8b2463fe27
d3da <daandieperink@gmail.com>
2025-05-20 12:09:17 +0200

    Implement temporal contrastive loss

--- foley2025/contrastive/v2/loss.py
+++ foley2025/contrastive/v2/loss.py
@@ -34,7 +34,19 @@ class TemporalInfoNCELoss(torch.nn.Module):
         self.softmax_temperature = softmax_temperature
 
     def forward(self,
-                a_encoder_embeddings,
-                v_encoder_embeddings):
-        # TODO
-        return torch.tensor(0)
+                a_encoder_embeddings,  # B x Ca x D
+                v_encoder_embeddings):  # B x Cv x D
+        batch_size, segments_per_clip, _ = a_encoder_embeddings.shape
+
+        sim_matrix = F.cosine_similarity(a_encoder_embeddings.unsqueeze(2),  # B x Ca x 1 x D
+                                         v_encoder_embeddings.unsqueeze(1),  # B x 1 x Cv x D
+                                         dim=-1)  # B x Ca x Cv
+        sim_matrix /= self.softmax_temperature
+        identity = torch.eye(segments_per_clip, device=sim_matrix.device)
+
+        audio_prob_logits = F.log_softmax(sim_matrix, dim=2)
+        video_prob_logits = F.log_softmax(sim_matrix, dim=1)
+
+        audio_loss = -audio_prob_logits.mul(identity).sum()
+        video_loss = -video_prob_logits.mul(identity).sum()
+        return audio_loss + video_loss
